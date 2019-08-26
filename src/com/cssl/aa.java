b9a52df836bc79abb9ab60f5b7087ca815885488
zhuw-aqu <1173260868@qq.com>
2019-08-26 11:52:10 +0800

    分享

---
+++ src/com/cssl/aa.java
@@ -0,0 +1,11 @@
+package com.cssl;
+
+public class aa {
+    public static void main(String [] arag){
+        System.out.println("abc");
+		System.out.println("bbb");
+		System.out.println("ccc");
+        System.out.println("dddd");
+        System.out.println("mmm");
+    }
+}
